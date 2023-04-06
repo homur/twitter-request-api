6fab8acbbf91c0656502642062488dba3ecfdd5f
Onur Atalay <onuratalay@gmail.com>
2023-04-06 21:05:19 +0200

    removing node_modules, adding gitignore

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  consumer_key: "xgt878uVUgiO1jwNUDKktMVjq",
-  consumer_secret: "d04NHOOX2dYOsyai4dKZf4s7WF0BPbWOKRjtsqdIPyIMyZtc5m",
-  access_token: "18559471-1PN6EE9LINO7boehHKXUIESpfmW9BxCi0qVTD1wPr",
-  access_token_secret: "V8WP5T3St8mVD7FsVxZgwtknL7KU9KqWa6N9LplrbyDp4",
-  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-  strictSSL: true, // optional - requires SSL certificates to be valid.
-};
